21594e4adb24b1e67116a3077b4bb478a61c1788
Anthony Hook <antone4hook@gmail.com>
2020-02-16 14:51:25 +0900

    chore: update packages and webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,41 @@
 // We are using node's native package 'path'
 // https://nodejs.org/api/path.html
-const path = require('path');
+const path = require("path");
 
-const webpack = require('webpack'); // reference to webpack Object
-
-// Including our UglifyJS
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const webpack = require("webpack"); // reference to webpack Object
 
 // using the newer beta version for >= Webpack 4
 // the current version is only good for <= Webpack 3
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const extractSass = new ExtractTextPlugin({
-    filename: "style.min.css",
+    filename: "style.min.css"
 });
 
 // Constant with our paths
 const paths = {
-    DIST: path.resolve(__dirname, 'dist'),
-    SRC: path.resolve(__dirname, 'src')
+    DIST: path.resolve(__dirname, "dist"),
+    SRC: path.resolve(__dirname, "src")
 };
 
 // Webpack configuration
 module.exports = {
-    entry: [
-        path.join(paths.SRC, 'index.js')
-    ],
+    entry: [path.join(paths.SRC, "index.js")],
     output: {
         path: paths.DIST,
-        filename: 'main.bundle.js'
+        filename: "main.bundle.js"
     },
-	// Adding jQuery as external library
-	externals: {
-	  jquery: 'jQuery'
-	},
+    // Adding jQuery as external library
+    externals: {
+        jquery: "jQuery"
+    },
     // Tell webpack to use html plugin -> ADDED IN THIS STEP
     // index.html is used as a template in which it'll inject bundled app.
     plugins: [
         new webpack.ProvidePlugin({
-          $: 'jquery',
-          jQuery: 'jquery',
-          Popper: 'popper.js'
+            $: "jquery",
+            jQuery: "jquery",
+            Popper: "popper.js"
         }),
-        new UglifyJSPlugin(),
         extractSass
     ],
     // Loaders configuration -> ADDED IN THIS STEP
@@ -51,46 +45,47 @@ module.exports = {
             {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
-                use: [
-                'babel-loader'
-                ],
+                use: ["babel-loader"]
             },
             {
-            	test: /\.scss$/,
-            	use: extractSass.extract({
-            		use: [{
-            			loader: "css-loader",
-            			options: {
-            				minimize: true
-            			}
-            		}, {
-            			loader: "sass-loader"
-            		}],
-					// use style-loader in development
-					fallback: "style-loader"
-				})
-            }, 
-            {
-            	test: /\.css$/,
-            	loader: 'style-loader',
+                test: /\.scss$/,
+                use: extractSass.extract({
+                    use: [
+                        {
+                            loader: "css-loader",
+                            options: {
+                                minimize: true
+                            }
+                        },
+                        {
+                            loader: "sass-loader"
+                        }
+                    ],
+                    // use style-loader in development
+                    fallback: "style-loader"
+                })
             },
             {
-            	test: /\.css$/,
-            	loader: 'css-loader',
-            	options: {
-            		minimize: true
-            	}
+                test: /\.css$/,
+                loader: "style-loader"
+            },
+            {
+                test: /\.css$/,
+                loader: "css-loader",
+                options: {
+                    minimize: true
+                }
             }
-        ],
+        ]
     },
-      // Enable importing JS files without specifying their's extenstion -> ADDED IN THIS STEP
-      //
-      // So we can write:
-      // import MyComponent from './my-component';
-      //
-      // Instead of:
-      // import MyComponent from './my-component.jsx';
-      resolve: {
-        extensions: ['.js', '.jsx'],
-    },
+    // Enable importing JS files without specifying their's extenstion -> ADDED IN THIS STEP
+    //
+    // So we can write:
+    // import MyComponent from './my-component';
+    //
+    // Instead of:
+    // import MyComponent from './my-component.jsx';
+    resolve: {
+        extensions: [".js", ".jsx", ".scss"]
+    }
 };
