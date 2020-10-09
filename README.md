fff46d0a44450a4005ffa67240bac4fcb540b1f8
Anthony Hook <maki@maki-no-MacBook-Pro.local>
2020-10-09 11:43:13 +0900

    chore: Dependabot warnings fixed, added watch: true to Webpack config

--- README.md
+++ README.md
@@ -1,42 +1,40 @@
 # wp-bootstrap4-sass
-A clean slate Wordpress theme template with Bootstrap(4), Sass, and a Webpack config with Babel.
-*This can be a decent starting point for simple theme development.*
+
+A clean slate Wordpress theme template with Bootstrap(4), Sass, and a Webpack/Babel config.
+_This can be a decent starting point for simple theme development._
 
 ## Installation
 
-* Clone or download into your themes folder
-* Using command line(Terminal or Command Prompt) navigate to wp-bootstrap4-sass directory
-* *npm update --save-dev*  to update all devDependencies
-* Run *npm update*
-* Run *npm start* for Webpack development build
-* Run *npm run build* for Webpack production build
+- Clone or download into your themes folder
+- Using command line(Terminal or Command Prompt) navigate to wp-bootstrap4-sass directory
+- Run `npm install`
+- Run _npm start_ for Webpack development build
+- Run `npm run build` for Webpack production build
+- Webpack config has `watch: true` for live compiling of JS and Sass
 
 ## Dev Dependencies
 
-*Note:* Using the extract-text-webpack-plugin Beta version since it plays better with Webpack 4; *npm i -D extract-text-webpack-plugin@next*
-
-* [Babel](https://babeljs.io/)
-* [babel-loader](https://github.com/babel/babel-loader)
-* [babel-preset-env](https://github.com/babel/babel/tree/master/packages/babel-preset-env)
-* [css-loader](https://www.npmjs.com/package/css-loader)
-* [extract-text-webpack-plugin ^4.0.0-beta.0](https://github.com/webpack-contrib/extract-text-webpack-plugin)
-* [node-sass](https://www.npmjs.com/package/node-sass)
-* [sass-loader](https://www.npmjs.com/package/sass-loader)
-* [style-loader](https://www.npmjs.com/package/style-loader)
-* [uglifyjs-webpack-plugin](https://www.npmjs.com/package/uglifyjs-webpack-plugin)
-* [webpack](https://webpack.js.org/)
-* [webpack-cli](https://webpack.js.org/api/cli/)
+- [Babel](https://babeljs.io/)
+- [babel-loader](https://github.com/babel/babel-loader)
+- [babel-preset-env](https://github.com/babel/babel/tree/master/packages/babel-preset-env)
+- [css-loader](https://www.npmjs.com/package/css-loader)
+- [extract-text-webpack-plugin ^4.0.0-beta.0](https://github.com/webpack-contrib/extract-text-webpack-plugin)
+- [node-sass](https://www.npmjs.com/package/node-sass)
+- [sass-loader](https://www.npmjs.com/package/sass-loader)
+- [style-loader](https://www.npmjs.com/package/style-loader)
+- [uglifyjs-webpack-plugin](https://www.npmjs.com/package/uglifyjs-webpack-plugin)
+- [webpack](https://webpack.js.org/)
+- [webpack-cli](https://webpack.js.org/api/cli/)
 
 ## Dependencies
 
-* [Bootstrap](https://getbootstrap.com/)
-* [Popper.js](https://popper.js.org/)
+- [Bootstrap](https://getbootstrap.com/)
+- [Popper.js](https://popper.js.org/)
 
 ## Acknowledgements
 
-* [HTML 5 Blank](https://github.com/toddmotto/html5blank)
+- [HTML 5 Blank](https://github.com/toddmotto/html5blank)
 
 ## License
 
 MIT
-
--- webpack.config.js
+++ webpack.config.js
@@ -8,84 +8,85 @@ const webpack = require("webpack"); // reference to webpack Object
 // the current version is only good for <= Webpack 3
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const extractSass = new ExtractTextPlugin({
-    filename: "style.min.css"
+	filename: "style.min.css",
 });
 
 // Constant with our paths
 const paths = {
-    DIST: path.resolve(__dirname, "dist"),
-    SRC: path.resolve(__dirname, "src")
+	DIST: path.resolve(__dirname, "dist"),
+	SRC: path.resolve(__dirname, "src"),
 };
 
 // Webpack configuration
 module.exports = {
-    entry: [path.join(paths.SRC, "index.js")],
-    output: {
-        path: paths.DIST,
-        filename: "main.bundle.js"
-    },
-    // Adding jQuery as external library
-    externals: {
-        jquery: "jQuery"
-    },
-    // Tell webpack to use html plugin -> ADDED IN THIS STEP
-    // index.html is used as a template in which it'll inject bundled app.
-    plugins: [
-        new webpack.ProvidePlugin({
-            $: "jquery",
-            jQuery: "jquery",
-            Popper: "popper.js"
-        }),
-        extractSass
-    ],
-    // Loaders configuration -> ADDED IN THIS STEP
-    // We are telling webpack to use "babel-loader" for .js and .jsx files
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: ["babel-loader"]
-            },
-            {
-                test: /\.scss$/,
-                use: extractSass.extract({
-                    use: [
-                        {
-                            loader: "css-loader",
-                            options: {
-                                minimize: true
-                            }
-                        },
-                        {
-                            loader: "sass-loader"
-                        }
-                    ],
-                    // use style-loader in development
-                    fallback: "style-loader"
-                })
-            },
-            {
-                test: /\.css$/,
-                loader: "style-loader"
-            },
-            {
-                test: /\.css$/,
-                loader: "css-loader",
-                options: {
-                    minimize: true
-                }
-            }
-        ]
-    },
-    // Enable importing JS files without specifying their's extenstion -> ADDED IN THIS STEP
-    //
-    // So we can write:
-    // import MyComponent from './my-component';
-    //
-    // Instead of:
-    // import MyComponent from './my-component.jsx';
-    resolve: {
-        extensions: [".js", ".jsx", ".scss"]
-    }
+	entry: [path.join(paths.SRC, "index.js")],
+	output: {
+		path: paths.DIST,
+		filename: "main.bundle.js",
+	},
+	watch: true,
+	// Adding jQuery as external library
+	externals: {
+		jquery: "jQuery",
+	},
+	// Tell webpack to use html plugin -> ADDED IN THIS STEP
+	// index.html is used as a template in which it'll inject bundled app.
+	plugins: [
+		new webpack.ProvidePlugin({
+			$: "jquery",
+			jQuery: "jquery",
+			Popper: "popper.js",
+		}),
+		extractSass,
+	],
+	// Loaders configuration -> ADDED IN THIS STEP
+	// We are telling webpack to use "babel-loader" for .js and .jsx files
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: ["babel-loader"],
+			},
+			{
+				test: /\.scss$/,
+				use: extractSass.extract({
+					use: [
+						{
+							loader: "css-loader",
+							options: {
+								minimize: true,
+							},
+						},
+						{
+							loader: "sass-loader",
+						},
+					],
+					// use style-loader in development
+					fallback: "style-loader",
+				}),
+			},
+			{
+				test: /\.css$/,
+				loader: "style-loader",
+			},
+			{
+				test: /\.css$/,
+				loader: "css-loader",
+				options: {
+					minimize: true,
+				},
+			},
+		],
+	},
+	// Enable importing JS files without specifying their's extenstion -> ADDED IN THIS STEP
+	//
+	// So we can write:
+	// import MyComponent from './my-component';
+	//
+	// Instead of:
+	// import MyComponent from './my-component.jsx';
+	resolve: {
+		extensions: [".js", ".jsx", ".scss"],
+	},
 };
