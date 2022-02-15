0f26937ba41278016bbab77229934526bbacece5
Anthony Hook <maki@maki-no-MacBook-Pro.local>
2022-02-15 15:22:48 +0900

    chore: update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,8 @@
 const path = require("path");
 
 const webpack = require("webpack"); // reference to webpack Object
-
-// using the newer beta version for >= Webpack 4
-// the current version is only good for <= Webpack 3
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const extractSass = new ExtractTextPlugin({
-	filename: "style.min.css",
-});
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const devMode = process.env.NODE_ENV !== "production";
 
 // Constant with our paths
 const paths = {
@@ -37,45 +32,32 @@ module.exports = {
 			jQuery: "jquery",
 			Popper: "popper.js",
 		}),
-		extractSass,
+		new MiniCssExtractPlugin({
+			filename: "style.min.css",
+		}),
 	],
 	// Loaders configuration -> ADDED IN THIS STEP
 	// We are telling webpack to use "babel-loader" for .js and .jsx files
 	module: {
 		rules: [
 			{
-				test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
-				use: ["babel-loader"],
-			},
-			{
-				test: /\.scss$/,
-				use: extractSass.extract({
-					use: [
-						{
-							loader: "css-loader",
-							options: {
-								minimize: true,
-							},
-						},
-						{
-							loader: "sass-loader",
-						},
-					],
-					// use style-loader in development
-					fallback: "style-loader",
-				}),
-			},
-			{
-				test: /\.css$/,
-				loader: "style-loader",
-			},
-			{
-				test: /\.css$/,
-				loader: "css-loader",
-				options: {
-					minimize: true,
+				test: /\.m?js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-env"],
+					},
 				},
+			},
+			{
+				test: /\.(sa|sc|c)ss$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					"css-loader",
+					"postcss-loader",
+					"sass-loader",
+				],
 			},
 		],
 	},
